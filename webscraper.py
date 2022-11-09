5dbd42df7f9b306183f04680c6b454b5d0a6ad5a
hisham <hisham96assi@gmail.com>
2022-11-08 20:34:08 -0500

    initializing

---
+++ webscraper.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+import requests
+from csv import writer
+import time
+import random
+from lxml import etree as et
+from selenium import driver
+from selenium.webdriver.common.keys import Keys
+
+driver.get("https://housesigma.com/web/en/recommend/more/justsold")
+
+
+
+
