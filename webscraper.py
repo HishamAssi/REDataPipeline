8b186eea60376aea475bc2c42dc5db2a909515b5
hisham <hisham96assi@gmail.com>
2022-11-11 22:27:57 -0500

    Modified sleep to selenium's webdriver timeout.
    Next steps:
    - Transfer files to s3.

--- webscraper.py
+++ webscraper.py
@@ -11,6 +11,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import json
 import browser_cookie3
 from login_credentials import username,password
@@ -20,54 +23,61 @@ from login_credentials import username,password
 class HouseSigmaScraper:
     states: Dict[str, str] = {'sold': 'justsold', 'listed': 'newlylisted'}
 
-    def __init__(self, state='sold'):
+    def __init__(self, state='sold', timeout=5):
         self.url = "https://housesigma.com/web/en/recommend/more/{}".format(self.states[state])
         chrome_options = Options()
         chrome_options.add_argument("user-data-dir=selenium")
         self.driver=webdriver.Chrome(options=chrome_options)
         self.state = state
+        self.timeout = timeout
 
 
     def login(self):
+        try:
+            print("logging in")
+            self.driver.get(self.url)
+            self.driver.find_element(By.XPATH, "//*[@id=\"app\"]/div/div[1]/div[1]/div[2]/div[3]/a[1]").\
+                click()
+            time.sleep(2)
+            self.driver.find_element(By.XPATH, "//*[@id=\"pane-email\"]/form/div[1]/div/div/input").\
+                send_keys(username)
+            self.driver.find_element(By.XPATH, "//*[@id=\"pane-email\"]/form/div[2]/div/div/input"). \
+                send_keys(password)
+            self.driver.find_element(By.XPATH, "//*[@id=\"login\"]/div[2]/div/div[3]/button").click()
+        except:
+            print("Login not needed")
+    def get_all_data(self):
+        print("getting data")
         self.driver.get(self.url)
-        time.sleep(5)
-        self.driver.find_element(By.XPATH, "//*[@id=\"app\"]/div/div[1]/div[1]/div[2]/div[3]/a[1]").\
-            click()
-        time.sleep(2)
-        self.driver.find_element(By.XPATH, "//*[@id=\"pane-email\"]/form/div[1]/div/div/input").\
-            send_keys(username)
-        self.driver.find_element(By.XPATH, "//*[@id=\"pane-email\"]/form/div[2]/div/div/input"). \
-            send_keys(password)
-        self.driver.find_element(By.XPATH, "//*[@id=\"login\"]/div[2]/div/div[3]/button").click()
-        time.sleep(2)
-
-    def get_data_list(self):
-        self.driver.get(self.url)
-        time.sleep(5)
+        try:
+            element_present = EC.presence_of_element_located((By.CLASS_NAME, 'el-card__body'))
+            WebDriverWait(self.driver, self.timeout).until(element_present)
+        except TimeoutException:
+            print("Timed out waiting for page to load")
         soup = BeautifulSoup(self.driver.page_source, 'lxml')
         all_links = soup.select('div.el-card__body')
+        print(all_links)
         all_listings = []
         domain = "https://housesigma.com"
         for div in all_links:
-
-            print(div.find('a')['href'])
             all_listings.append(self.get_data(domain+div.find('a')['href']))
 
         return all_listings
 
     def get_data(self, listing_url):
-
+        print("getting data for: " + listing_url)
         self.driver.get(listing_url)
-        time.sleep(5)
+        try:
+            element_present = EC.presence_of_element_located((By.CLASS_NAME, 'item'))
+            WebDriverWait(self.driver, self.timeout).until(element_present)
+        except TimeoutException:
+            print("Timed out waiting for page to load")
         soup=BeautifulSoup(self.driver.page_source, 'lxml')
         items = {"link": listing_url}
 
         for div in soup.select('div.item'):
             if(div.find("h2") and div.find("span")):
                 items[div.find("h2").string] = div.find("span").string
-            print(div)
-            print(div.find("h2"))
-            print(div.find("span"))
             # print(div.find())
         return items
 
@@ -85,3 +95,10 @@ class HouseSigmaScraper:
 
 # test_url = "https://housesigma.com/web/en/house/aD6p781zvPr3wRQr/6680-93-Hwy-County-Rd-Tay-L0K2E0-S5818021-S5818021-40343185"
 scraper = HouseSigmaScraper('sold')
+scraper.login()
+data = scraper.get_all_data()
+scraper.write_data_to_file(data)
+
+
+
+
