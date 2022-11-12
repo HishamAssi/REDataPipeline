b1382df2a53ff0f99561b907dd29401de7b57a25
hisham <hisham96assi@gmail.com>
2022-11-12 13:14:04 -0500

    starting kafka producer

---
+++ kafka_producer.py
@@ -0,0 +1,11 @@
+import confluent_kafka
+import requests
+import KafkaDevConfig
+import json
+from kafka import KafkaProducer
+
+class listing_kafka_producer():
+
+
+
+# cert location: '/Users/hisham/PycharmProjects/pythonProject/venv/lib/python3.10/site-packages/certifi/cacert.pem'
