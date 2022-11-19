7e1c9b8c9d17722edf505d98503fbce3489dd9ce
hisham <hisham96assi@gmail.com>
2022-11-18 22:41:56 -0500

    Postgres RDS created and initial setup steps started. Need to figure out an easy way to created the ddl.

--- kafkaBased/consumeKafkaToSQL.py
+++ kafkaBased/consumeKafkaToSQL.py
@@ -6,10 +6,12 @@ import json
 import certifi
 import psycopg2
 import sqlalchemy
+import login_credentials
+import re
 
 class postgres_write():
 
-    def __init__(self, hostname, port=5432, username, password):
+    def __init__(self):
         """
 
         :param hostname:
@@ -18,11 +20,33 @@ class postgres_write():
         :param password:
         """
         self.psql_engine = psycopg2.connect( database="postgres",
-                                             user="",
-                                             password="",
-                                             host="",
-                                             port=''
+                                             user=login_credentials.db_user,
+                                             password=login_credentials.db_password,
+                                             host=login_credentials.db_hostname,
+                                             port=login_credentials.db_port
                                              )
+
+    def create_ddl(self):
+        # transform json to dict
+        with open("/Users/hisham/PycharmProjects/pythonProject/venv/proj/data/extracted_sold.json", "r") as json_file:
+            lines = json_file.readlines()[0]
+            data = json.loads(json.loads(lines))
+        data_types = data.values()
+        column_names = data.keys()
+        ddl_types = []
+        for value in data_types:
+            if re.search('[a-zA-Z]', value):
+                ddl_types.append("varchar(255)")
+            elif '-' in value:
+                ddl_types.append("date")
+            elif '.' in value:
+                ddl_types.append("float")
+            elif re.search('[0-9]', value):
+                ddl_types.append("int")
+
+
+
+        print(data.values())
 
     def write_message(self, tablename, message):
         self.psql_engine
@@ -39,7 +63,7 @@ class kafka_consumer():
     def __init__(self, conf_path, topic):
         conf = kafka_producer.read_ccloud_config(conf_path)
         conf['group.id'] = 'he_consumer_1'
-        conf['auto.offset.reset'] = 'earliest'
+        # conf['auto.offset.reset'] = 'earliest'
         self.consumer = Consumer(conf)
         self.topic = topic
 
@@ -77,7 +101,8 @@ class kafka_consumer():
 
 
 if __name__ == "__main__":
-    consumer = kafka_consumer("/Users/hisham/PycharmProjects/pythonProject/venv/proj/credentials/KafkaDevConfig.properties",
-                              "housesigmascraper")
-    consumer.subscribe_to_topic()
-
+    # consumer = kafka_consumer("/Users/hisham/PycharmProjects/pythonProject/venv/proj/credentials/KafkaDevConfig.properties",
+    #                           "housesigmascraper")
+    # consumer.subscribe_to_topic()
+    postgres_con = postgres_write()
+    postgres_con.create_ddl()
